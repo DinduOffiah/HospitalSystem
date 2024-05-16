247ef03d3aa3bf72875e907c4b64372eaacb6fdd
Chidindu <chidinduoffiah@gmail.com>
2024-05-16 12:24:39 +0100

    Add Controllers

--- RegisterService/Program.cs
+++ RegisterService/Program.cs
@@ -1,10 +1,16 @@
 using AppDbContext.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using RegisterService.Interface;
+using RegisterService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddScoped<IPatientService, PatientService>();
+
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
