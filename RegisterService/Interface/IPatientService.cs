d6bad4f42fba8cb9daf07a55516533efce58e7ac
Chidindu <chidinduoffiah@gmail.com>
2024-05-16 11:15:17 +0100

    Contract

---
+++ RegisterService/Interface/IPatientService.cs
@@ -0,0 +1,9 @@
+﻿using RegisterService.Models;
+
+namespace RegisterService.Interface
+{
+    public interface IPatientService
+    {
+        Task<Patient> RegisterPatientAsync(Patient patient);
+    }
+}
