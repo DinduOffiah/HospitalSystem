3c139774c1c7caf04f2aeba97910ef3a41e932b4
Chidindu <chidinduoffiah@gmail.com>
2024-05-16 11:20:17 +0100

    Service

---
+++ RegisterService/Services/PatientService.cs
@@ -0,0 +1,25 @@
+﻿using AppDbContext.Data;
+using RegisterService.Interface;
+using RegisterService.Models;
+
+namespace RegisterService.Services
+{
+    public class PatientService : IPatientService
+    {
+        private readonly RegisterDbContext _context;
+
+        public PatientService(RegisterDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Patient> RegisterPatientAsync(Patient patient)
+        {
+            // Add the patient to the database
+            _context.Patients.Add(patient);
+            await _context.SaveChangesAsync();
+
+            return patient;
+        }
+    }
+}
