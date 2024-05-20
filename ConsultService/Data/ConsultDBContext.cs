a9370d9926e40e17e730fb8e350dfc13a734cc6c
Chidindu <chidinduoffiah@gmail.com>
2024-05-20 12:35:35 +0100

    DB Context

---
+++ ConsultService/Data/ConsultDBContext.cs
@@ -0,0 +1,15 @@
+﻿using ConsultService.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace ConsultService.Data
+{
+    public class ConsultDBContext : DbContext
+    {
+        public ConsultDBContext(DbContextOptions<ConsultDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Doctor> Doctors { get; set; }
+    }
+}
--- ConsultService/Services/DoctorService.cs
+++ ConsultService/Services/DoctorService.cs
@@ -1,6 +1,15 @@
-﻿namespace ConsultService.Services
+﻿using ConsultService.Models;
+
+namespace ConsultService.Services
 {
     public class DoctorService
     {
+        public async Task<Doctor> RegisterPatientAsync(Doctor doctor)
+        {
+            _context.Patients.Add(patient);
+            await _context.SaveChangesAsync();
+
+            return patient;
+        }
     }
 }
