5c67147e4ec5c6798bfaf763afc29a881df7f311
Chidindu <chidinduoffiah@gmail.com>
2024-05-20 10:56:49 +0100

    Model

---
+++ ConsultService/Models/Patient.cs
@@ -0,0 +1,10 @@
+﻿namespace RegisterService.Models
+{
+    public class Patient
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+    }
+}
