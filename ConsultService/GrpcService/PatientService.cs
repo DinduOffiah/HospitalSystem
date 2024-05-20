4e82af522add62f093190109eab4f1b8c193f0af
Chidindu <chidinduoffiah@gmail.com>
2024-05-20 21:19:56 +0100

    Grpc Installation

---
+++ ConsultService/GrpcService/PatientService.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Core;
+
+namespace ConsultService.Protos
+{
+    public class PatientService : Patient.PatientBase
+    {
+        public override Task<PatientResponse> UpdatePatient(PatientRequest request, ServerCallContext context)
+        {
+            // Update the patient in the database here
+
+            return Task.FromResult(new PatientResponse
+            {
+                Message = "Patient updated successfully"
+            });
+        }
+    }
+
+}
