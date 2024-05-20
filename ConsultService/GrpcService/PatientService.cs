dba4ff7baca168fef41d7b0e42f87c1f3d1df4b0
Chidindu <chidinduoffiah@gmail.com>
2024-05-20 21:40:30 +0100

    Patient Service

--- ConsultService/GrpcService/PatientService.cs
+++ ConsultService/GrpcService/PatientService.cs
@@ -6,7 +6,8 @@ namespace ConsultService.Protos
     {
         public override Task<PatientResponse> UpdatePatient(PatientRequest request, ServerCallContext context)
         {
-            // Update the patient in the database here
+            _context.Entry(patient).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
 
             return Task.FromResult(new PatientResponse
             {
---
+++ ConsultService/Services/PatientService.cs
@@ -0,0 +1,24 @@
+﻿using ConsultService.Data;
+using Microsoft.EntityFrameworkCore;
+using RegisterService.Models;
+
+namespace ConsultService.Services
+{
+    public class PatientService
+    {
+        private readonly ConsultDBContext _context;
+
+        public PatientService(ConsultDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Patient> UpdatePatientAsync(Patient patient)
+        {
+            _context.Entry(patient).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return patient;
+        }
+    }
+}
