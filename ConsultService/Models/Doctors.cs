613cff0591c57d206dbee09eeabeb068803d60ee
Chidindu <chidinduoffiah@gmail.com>
2024-05-20 11:03:55 +0100

    Doctors Controller

---
+++ ConsultService/Models/Doctors.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsultService.Models
+{
+    public class Doctors
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int YearsOfExperience { get; set; }
+    }
+}
